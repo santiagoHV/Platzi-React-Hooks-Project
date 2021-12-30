1d1da7becc6829db49197ad2a192cad19a01d325
SantiagoHV <sherrerav@correo.udistrital.edu.co>
2021-12-30 16:26:37 -0500

    view done

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,9 +1,75 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import '../styles/components/Information.css';
 
 const Information = () => {
     return (
-        <div>
-            <h1>Information</h1>
+        <div className="Information">
+            <div className="Information-content">
+                <div className="Information-head">
+                    <h2>Información de contacto</h2>
+                </div>
+                <div className="Information-form">
+                    <form action="">
+                        <input 
+                            type="text" 
+                            placeholder="Nombre completo"
+                            name="name" />
+                        <input 
+                            type="text" 
+                            placeholder="Correo electrónico"
+                            name="email" />
+                        <input 
+                            type="text" 
+                            placeholder="Dirección"
+                            name="address" />
+                        <input 
+                            type="text" 
+                            placeholder="Apto, Casa, Oficina"
+                            name="apto" />
+                        <input 
+                            type="text" 
+                            placeholder="Ciudad"
+                            name="city" />
+                        <input 
+                            type="text" 
+                            placeholder="Pais"
+                            name="country" />
+                        <input 
+                            type="text" 
+                            placeholder="Estado o departamento"
+                            name="state" />
+                        <input 
+                            type="text" 
+                            placeholder="Código postal"
+                            name="cp" />
+                        <input 
+                            type="text" 
+                            placeholder="Teléfono"
+                            name="phone" />
+                    </form>
+                </div>
+                <div className="Information-buttons">
+                    <div className="Information-back">
+                        Regresar
+                    </div>
+                    <Link to='/checkout/payment'>
+                        <div className="Information-next">
+                            Pagar
+                        </div>
+                    </Link>
+                   
+                </div>
+            </div>
+            <div className="Information-sidebar">
+                <h3>Pedido:</h3>
+                <div className="Information-item">
+                    <div className="Information-element">
+                        <h4>ITEM name</h4>
+                        <span>$10</span>      
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -15,8 +15,8 @@ const App = () => {
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/checkout" component={Checkout} />
-                <Route exact path="/checkout/information" component={Information} />
-                <Route exact path="checkout/payment/" component={Payment} />
+                <Route exact path="/checkout/information/" component={Information} />
+                <Route exact path="/checkout/payment/" component={Payment} />
                 <Route exact path="/checkout/success" component={Success} />
                 <Route component={NotFound} />
             </Switch>
