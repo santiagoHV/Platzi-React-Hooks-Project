53cb7061f39c2346a1f4ab5e9e73dd69c37f1573
SantiagoHV <sherrerav@correo.udistrital.edu.co>
2022-01-02 12:51:11 -0500

    google maps integrated with errors for payment

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, {useContext} from "react";
+import Map from "../components/Map";
+import AppContext from "../context/AppContext";
+import useGoogleAddress from "../hooks/useGoogleAddress";
 import '../styles/components/Success.css';
 
 const Success = () => {
+    const {state} = useContext(AppContext);
+    const { buyer } = state;
+    console.log('buyer')
+    console.log(buyer[0].address);
+    // const location = useGoogleAddress(buyer[0].address);
+
+    
     return (
         <div className="Success">
             <div className="Success-content">
-                <h2>Gracias por tu compra</h2>
+                <h2>{`${buyer.name} Gracias por tu compra`}</h2>
                 <span>Tu pedido llegará en 3 dias a tu direccion</span>
                 <div className="Success-map">
-                    Google maps
+                    <Map/>
                 </div>
             </div>
         </div>
